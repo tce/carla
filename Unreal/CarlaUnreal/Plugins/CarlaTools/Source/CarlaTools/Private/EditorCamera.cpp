2405f8f651014587fdb9e3d0572cdb6e057e6c7a
Marcel Pi <marcelpi97@gmail.com>
2023-05-12 14:30:22 +0200

    Add EditorCameraUtils.

--- Unreal/CarlaUnreal/Plugins/CarlaTools/Source/CarlaTools/Private/EditorCamera.cpp
+++ Unreal/CarlaUnreal/Plugins/CarlaTools/Source/CarlaTools/Private/EditorCamera.cpp
@@ -1,18 +1,25 @@
+// Copyright (c) 2023 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
 #include "EditorCamera.h"
 #include "UnrealClient.h"
 #include "Editor/EditorEngine.h"
 #include "EditorViewportClient.h"
 
-void UEditorCameraUtils::Get()
+void AEditorCameraUtils::Get()
 {
-	auto ViewportClient = dynamic_cast<FEditorViewportClient*>(GEditor->GetActiveViewport()->GetClient());
-	Location = ViewportClient->GetViewLocation();
-	Rotation = ViewportClient->GetViewRotation();
+    auto ViewportClient = dynamic_cast<FEditorViewportClient*>(GEditor->GetActiveViewport()->GetClient());
+    CameraTransform = FTransform();
+    CameraTransform.SetLocation(ViewportClient->GetViewLocation());
+    CameraTransform.SetRotation(FQuat(ViewportClient->GetViewRotation()));
 }
 
-void UEditorCameraUtils::Set()
+void AEditorCameraUtils::Set()
 {
-	auto ViewportClient = dynamic_cast<FEditorViewportClient*>(GEditor->GetActiveViewport()->GetClient());
-	ViewportClient->SetViewLocation(Location);
-	ViewportClient->SetViewRotation(Rotation);
+    auto ViewportClient = dynamic_cast<FEditorViewportClient*>(GEditor->GetActiveViewport()->GetClient());
+    ViewportClient->SetViewLocation(CameraTransform.GetLocation());
+    ViewportClient->SetViewRotation(FRotator(CameraTransform.GetRotation()));
 }
